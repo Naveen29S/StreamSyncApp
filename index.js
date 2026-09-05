9bf086860633c1c84a5032bac085701d66f6e5f5
NAVEEN BALAJI <naveensujith31@gmail.com>
2026-09-05 16:34:33 +0530

    Fix entry point to expo-router and extend gitignore

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-import { registerRootComponent } from 'expo';
+// Expo Router entrypoint
+// Delegates root component registration to expo-router/entry to match package.json
+import 'expo-router/entry';
 
-import App from './App';
-
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
-registerRootComponent(App);
